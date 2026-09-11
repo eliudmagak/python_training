dff4e82954036039a584b36cc6cd75848bea7bd0
eliudmagak <eliudkondeyo@gmail.com>
2026-09-11 21:13:37 +0300

    beginning

---
+++ intro.py
@@ -0,0 +1,10 @@
+print("Eliud Magak")
+print("1000")
+
+total=100+500
+
+print(total)
+
+student_name="Alex"
+print(student_name)
+
---
+++ variables.py
@@ -0,0 +1,12 @@
+# calculate the area of a square
+
+side=50
+area=side*side
+print(area)
+
+# calculate the area of a rectangle
+
+length=10
+width=8
+area=length*width
+print(area)

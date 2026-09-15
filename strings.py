bea48ba029c3f9ff8c92da16d35439e0de13abe8
eliudmagak <eliudkondeyo@gmail.com>
2026-09-15 16:46:22 +0300

    python strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+first_name='Eliud'
+last_name='Magak'
+
+full_name=first_name+''+last_name
+print(full_name)
+
+num1=100
+num2=200
+
+total=num1+num2
+print(total)
+#When we add strings they concatenate, when we add numbers they sum up  
+
+text='I am a software developer'
+print(text[10])
+print(text[-11])
+
+text1='I am a student doing software development and we are learning python programming'
+print(text1[21:41])
+print(text1[62:80])
+print(text1[0:15])
+print(len(text1))

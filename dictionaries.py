b0161bca69b1cd36bec7b1489f67a3bd81d55b94
eliudmagak <eliudkondeyo@gmail.com>
2026-09-24 20:21:04 +0300

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -9,7 +9,9 @@ student1={
     'name':'alex',
       'age':20,
       'adm':'tech2015',
-      'city':'Nairobi'}
+      'city':'Nairobi',
+      'skills': ['UI', 'UX', 'Web']}
+
 print(student1)
 print(type(student1))
 
@@ -35,4 +37,22 @@ print(student1)
 
 #update name with a different name
 student1['name']='brian'
+print(student1)
+
+#Delete a oroperty
+del(student1['age'])
+print(student1)
+
+#.get() used to display values
+print(student1.get('adm'))
+
+#.keys Used to display a list of all keys
+print(student1.keys())
+
+#.values Used to display a list of all values
+print(student1.values())
+
+#.items Used to display list of tuples of each property
+print(student1.items())
+
 print(student1)
